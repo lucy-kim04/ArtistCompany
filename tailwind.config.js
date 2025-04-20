6bc4c44051c5d35209f515d6455ee0e2c9bda7b1
HJ <a01025105259@gmail.com>
2025-04-20 19:32:10 +0900

    fix: tailwind 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
