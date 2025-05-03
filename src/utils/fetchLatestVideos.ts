726e6a5a99c52077bf47da6f23a8707286edfeea
HJ <a01025105259@gmail.com>
2025-05-04 00:45:04 +0900

    fix: fetchlatestvideo 수정

--- src/utils/fetchLatestVideos.ts
+++ src/utils/fetchLatestVideos.ts
@@ -1,6 +1,19 @@
 const API_KEY = process.env.YOUTUBE_API_KEY!;
 const CHANNEL_ID = 'UCLTZj7RzpoUCG_cqD0NVHbw';
 
+type YoutubeAPIItem = {
+  id: { videoId: string };
+  snippet: {
+    title: string;
+    publishedAt: string;
+    thumbnails: {
+      medium: {
+        url: string;
+      };
+    };
+  };
+};
+
 export const fetchLatestVideos = async () => {
   const res = await fetch(
     `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&order=date&part=snippet&type=video&maxResults=12`
@@ -12,7 +25,7 @@ export const fetchLatestVideos = async () => {
     throw new Error(`YouTube API 응답에 items가 없음: ${JSON.stringify(data)}`);
   }
 
-  return data.items.map((item: any) => ({
+  return (data.items as YoutubeAPIItem[]).map((item) => ({
     id: item.id.videoId,
     title: item.snippet.title,
     thumbnail: item.snippet.thumbnails.medium.url,
